90c9dca4b92ef4f25b6a15716b6bb5e34696dcd5
JimHwkins <howlarrior@gmail.com>
2023-10-09 22:58:07 -0500

    feat: Add initial Dashboard component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,23 @@
+import Dashboard from "@/components/Dashboard";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
+import { db } from "@/db";
 
-const Page = () => {
+const Page = async () => {
   const { getUser } = getKindeServerSession();
   const user = getUser();
 
-  if(!user || !user.id) redirect("/auth-callback?origin=dashboard")
+  if (!user || !user.id) redirect("/auth-callback?origin=dashboard");
 
-  
+  const dbUser = await db.user.findFirst({
+    where: {
+      id: user.id,
+    },
+  });
 
-  return <div>{user.email}</div>;
+  if (!dbUser) redirect("/auth-callback?origin=dashboard");
+
+  return <Dashboard />;
 };
 
 export default Page;

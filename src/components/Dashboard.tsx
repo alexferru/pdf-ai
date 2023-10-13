b641ef3d245b734660886aae0daff19b5a2c61ad
JimHwkins <howlarrior@gmail.com>
2023-10-12 23:19:45 -0500

    feat: Add onSuccess function to deleteFile mutation

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -9,9 +9,15 @@ import { formatDistanceStrict, subDays } from "date-fns";
 import { Button } from "./ui/button";
 
 const Dashboard = () => {
+  const utils = trpc.useContext();
+
   const { data: files, isLoading } = trpc.getUserFiles.useQuery();
 
-  const { mutate: deleteFile } = trpc.deleteFile.useMutation();
+  const { mutate: deleteFile } = trpc.deleteFile.useMutation({
+    onSuccess: () => {
+      utils.getUserFiles.invalidate();
+    },
+  });
 
   return (
     <main className="mx-auto max-w-7xl md:p-10">

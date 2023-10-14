7440b54f945994fb111b2f000c6c7038449cd996
JimHwkins <howlarrior@gmail.com>
2023-10-13 23:09:47 -0500

    feat: Add authentication and file retrieval to dashboard/[fileid] route

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -1,16 +1,31 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { notFound, redirect } from "next/navigation";
+import { db } from "@/db";
+
 interface PageProps {
   params: {
     fileid: string;
   };
 }
 
-const Page = ({ params }: PageProps) => {
+const Page = async ({ params }: PageProps) => {
   const { fileid } = params;
-  
+
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
+
+  if (!user || user.id) redirect(`auth-callback?origin=dashboard/${fileid}`);
+
+  const file = await db.file.findFirst({
+    where: {
+      id: fileid,
+      userId: user.id,
+    },
+  });
+
+  if (!file) notFound();
 
   return <div>{fileid}</div>;
 };
 
 export default Page;
-
-Add the Page component to the dashboard/[fileid] route to display the details of a specific file.

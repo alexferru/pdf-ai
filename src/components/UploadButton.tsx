5ef18ebbec073654f0cda02d581cbff41a8a0de8
JimHwkins <howlarrior@gmail.com>
2023-10-15 23:25:56 -0500

    feat: Add startPolling function to UploadButton

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -8,13 +8,24 @@ import { Cloud, File } from "lucide-react";
 import { Progress } from "./ui/progress";
 import { useUploadThing } from "@/lib/uploadthing";
 import { useToast } from "./ui/use-toast";
+import { trpc } from "@/app/_trpc/client";
+import { useRouter } from "next/navigation";
 
 const UploadDropzone = () => {
+  const router = useRouter();
   const [isUploading, setIsUploading] = useState<boolean>(true);
   const [uploadProgress, setUploadProgress] = useState<number>(0);
   const { toast } = useToast();
 
   const { startUpload } = useUploadThing("pdfUploader");
+
+  const { mutate: startPolling } = trpc.getFile.useMutation({
+    onSuccess: (file) => {
+      router.push(`/dashboard/${file.id}`);
+    },
+    retry: true,
+    retryDelay: 500,
+  });
 
   const startSimulatedProgress = () => {
     setUploadProgress(0);
@@ -65,6 +76,8 @@ const UploadDropzone = () => {
 
         clearInterval(progressInterval);
         setUploadProgress(100);
+
+        startPolling({ key });
       }}
     >
       {({ getRootProps, getInputProps, acceptedFiles }) => (
@@ -105,6 +118,8 @@ const UploadDropzone = () => {
                   />
                 </div>
               ) : null}
+
+              <input type="file" id="dropzone-file" className="hidden" />
             </label>
           </div>
         </div>

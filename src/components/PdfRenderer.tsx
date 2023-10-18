be1a80e799dcba376d41ffba4af026c9b3336965
JimHwkins <howlarrior@gmail.com>
2023-10-17 22:24:29 -0500

    chore: Add input component using shadcn-ui

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { Loader2 } from "lucide-react";
+import { ChevronDown, Loader2 } from "lucide-react";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 import { useToast } from "./ui/use-toast";
 
 import { useResizeDetector } from "react-resize-detector";
+import { Button } from "./ui/button";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
@@ -22,7 +23,12 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
       <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
-        <div className="flex items-center gap-1.5">top bar</div>
+        <div className="flex items-center gap-1.5">
+          <Button variant="ghost" aria-label="previous page">
+            <ChevronDown className="h-4 w-4" />
+          </Button>
+          <div className="flex items-center gap-1.5"></div>
+        </div>
       </div>
 
       <div className="flex-1 w-full max-h-100 items-center ">
@@ -52,5 +58,3 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
 };
 
 export default PdfRenderer;
-
-

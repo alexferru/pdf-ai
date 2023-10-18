2a1e273a2dfb702eafa08006dff1230bbd893bbe
JimHwkins <howlarrior@gmail.com>
2023-10-17 21:48:40 -0500

    feat: Add user feedback to the PdfRenderer component to display a loading spinner while the PDF is loading and to display an error message if the PDF fails to load

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { Loader2 } from "lucide-react";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
+import { useToast } from "./ui/use-toast";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
@@ -11,6 +13,8 @@ interface PdfRendererProps {
 }
 
 const PdfRenderer = ({ url }: PdfRendererProps) => {
+  const { toast } = useToast();
+
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
       <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
@@ -19,7 +23,22 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
 
       <div className="flex-1 w-full max-h-100 items-center ">
         <div>
-          <Document file={url} className="max-h-full">
+          <Document
+            loading={
+              <div className="flex justify-center">
+                <Loader2 className="my-24 h-6 w-6 animate-spin" />
+              </div>
+            }
+            onLoadError={() => {
+              toast({
+                title: "Error loading PDF",
+                description: "Something went wrong",
+                variant: "destructive",
+              });
+            }}
+            file={url}
+            className="max-h-full"
+          >
             <Page pageNumber={1} />
           </Document>
         </div>

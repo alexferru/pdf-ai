abd67a1e364b8dd8c14baef56a3fc075bc2d3ed8
JimHwkins <howlarrior@gmail.com>
2023-10-19 22:53:01 -0500

    feat: Add a file upload status query to the ChatWrapper component to display a loading message while the file is being processed

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -2,7 +2,7 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { notFound, redirect } from "next/navigation";
 import { db } from "@/db";
 import PdfRenderer from "@/components/PdfRenderer";
-import ChatWrapper from "@/components/ChatWrapper";
+import ChatWrapper from "@/components/chat/ChatWrapper";
 
 interface PageProps {
   params: {

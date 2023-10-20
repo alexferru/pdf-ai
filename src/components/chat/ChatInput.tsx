0017dd0887e085331591ef93103145a2b397fd39
JimHwkins <howlarrior@gmail.com>
2023-10-19 23:20:35 -0500

    style: Add initial styling to ChatInput component

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -3,7 +3,17 @@ interface ChatInputProps {
 }
 
 const ChatInput = ({ isDisabled }: ChatInputProps) => {
-  return <div></div>;
+  return (
+    <div className="absolute bottom-0 left-0 w-full">
+      <form className="mx-2  flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:max-w-2xl xl:max-w-3xl">
+        <div className="relative flex h-full flex-1 items-stretch md:flex-col">
+          <div className="relative flex flex-col w-full flex-grow p-4">
+            <div className="relative"></div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default ChatInput;

a90ea7a1cfd0e01a83965a6db073dca7d38deb10
JimHwkins <howlarrior@gmail.com>
2023-10-20 22:41:15 -0500

    feat: Add PineconeStore indexing to the file upload process to enable the app to vectorize and index uploaded documents

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -2,6 +2,11 @@ import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
+import { pinecone } from "@/lib/pinecone";
+
 const f = createUploadthing();
 
 export const ourFileRouter = {
@@ -30,7 +35,43 @@ export const ourFileRouter = {
           `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
         );
         const blob = await response.blob();
-      } catch (err) {}
+        const loader = new PDFLoader(blob);
+
+        const pageLevelDocs = await loader.load();
+
+        const pagesAmt = pageLevelDocs.length;
+
+        // vectorize and index document
+
+        const pineconeIndex = pinecone.Index("dkmnt");
+
+        const embeddings = new OpenAIEmbeddings({
+          openAIApiKey: process.env.OPENAI_API_KEY,
+        });
+
+        await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
+          pineconeIndex,
+          namespace: createdFile.id,
+        });
+
+        await db.file.update({
+          data: {
+            uploadStatus: "SUCCESS",
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+      } catch (err) {
+        await db.file.update({
+          data: {
+            uploadStatus: "FAILED",
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+      }
     }),
 } satisfies FileRouter;
 

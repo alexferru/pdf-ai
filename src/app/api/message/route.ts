a30de4a6fb84c27003f6b3e7bf61ceaadc18cf29
JimHwkins <howlarrior@gmail.com>
2023-10-23 21:35:59 -0500

    Add PineconeStore vectorization to the route to enable the app to vectorize messages

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,7 +1,22 @@
 import { db } from "@/db";
 import { sendMessageValidator } from "@/lib/validators/SendMessageValidator";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { NextRequest } from "next/server";
+import { pinecone } from "@/lib/pinecone";
+import { Pinecone } from "@pinecone-database/pinecone";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
+
+interface SearchResult {
+  pageContent: string;
+  metadata: {
+    fileName: string;
+  };
+}
+
+function customFilter(result: SearchResult, targetFileName: string): boolean {
+  return result.metadata?.fileName === targetFileName;
+}
 
 export const POST = async (req: NextRequest) => {
   const body = await req.json();
@@ -33,5 +48,14 @@ export const POST = async (req: NextRequest) => {
     },
   });
 
-  
+  //vectorize message
+  const embeddings = new OpenAIEmbeddings({
+    openAIApiKey: process.env.OPENAI_API_KEY,
+  });
+
+  const pineconeIndex = pinecone.Index("dkmnt1");
+
+  const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+    pineconeIndex,
+  });
 };

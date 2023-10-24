6084e9a78df140099ada5e146e0a1c98f3986c89
JimHwkins <howlarrior@gmail.com>
2023-10-23 22:31:04 -0500

    feat: Add OpenAI chat completions to the message route to enable the app to generate responses to user messages

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -4,8 +4,10 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { NextRequest } from "next/server";
 import { pinecone } from "@/lib/pinecone";
-import { Pinecone } from "@pinecone-database/pinecone";
 import { PineconeStore } from "langchain/vectorstores/pinecone";
+
+import { OpenAIStream, StreamingTextResponse } from "ai";
+import { openai } from "@/lib/openai";
 
 interface SearchResult {
   pageContent: string;
@@ -58,4 +60,66 @@ export const POST = async (req: NextRequest) => {
   const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
     pineconeIndex,
   });
+
+  try {
+    // Search for similar messages using the file ID as context
+    const results = await vectorStore.similaritySearch(message, 1, {
+      filter: (result: SearchResult) => customFilter(result, file.id),
+    });
+    const prevMessages = await db.message.findMany({
+      where: { fileId },
+      orderBy: { createdAt: "asc" },
+      take: 6,
+    });
+    const formattedPrevMessages = prevMessages.map((msg) => ({
+      role: msg.isUserMessage ? "user" : "assistant",
+      content: msg.text,
+    }));
+
+    // Construct a context string with previous conversation, results, and user input
+    const context = `PREVIOUS CONVERSATION:${formattedPrevMessages.map(
+      (msg) => {
+        if (msg.role === "user") return `User:${msg.content}\n`;
+        return `Assistant:${msg.content}\n`;
+      }
+    )}CONTEXT:${results
+      .map((r) => r.pageContent)
+      .join("\n\n")}USER INPUT:${message}`;
+
+    // Use a system message to instruct the model
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      temperature: 0.7, // Adjust the temperature as needed
+      stream: true,
+      messages: [
+        {
+          role: "system",
+          content:
+            "You have access to a PDF document. Please use the information from the document to answer the user's question.",
+        },
+        {
+          role: "user",
+          content: context, // Provide the context here
+        },
+      ],
+    });
+
+    const stream = OpenAIStream(response, {
+      async onCompletion(completion) {
+        await db.message.create({
+          data: {
+            text: completion,
+            isUserMessage: false,
+            fileId,
+            userId,
+          },
+        });
+      },
+    });
+
+    return new StreamingTextResponse(stream);
+  } catch (error) {
+    console.error("Error searching for similar messages:", error);
+    return new Response("InternalServerError", { status: 500 });
+  }
 };

2f27083a8aa679df49131ee34355770458f8bd69
JimHwkins <howlarrior@gmail.com>
2023-10-24 23:32:56 -0500

    feat: Add ExtendedMessage type for chat messages

--- src/types/message.ts
+++ src/types/message.ts
@@ -4,3 +4,11 @@ import { inferRouterOutputs } from "@trpc/server";
 type RouterOutput = inferRouterOutputs<AppRouter>;
 
 type Messages = RouterOutput["getFileMessages"]["messages"];
+
+type OmitText = Omit<Messages[number], "text">;
+
+type ExtendedText = {
+  text: string | JSX.Element;
+};
+
+export type ExtendedMessage = OmitText & ExtendedText;
